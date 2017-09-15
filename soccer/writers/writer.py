efc49bcac65b79d745cc8076aa9b9deb335d5705
Hendrik Hilleckes <hendrik.hilleckes@leogistics.com>
2017-09-16 00:59:15 +0200

    added basic writer to output table

---
+++ soccer/writers/writer.py
@@ -0,0 +1,12 @@
+""" Basic writer """
+class Writer(object):
+    """
+    Basic writer
+    """
+    def __init__(self):
+        pass
+
+    def league_table(self, standings):
+        print("    %-25s %2s %2s %2s %2s %7s %3s" % ("Team", "P", "W", "D", "L", "Goals","Pts"))
+        for team in standings["standing"]:
+            print("%2s. %-25s %2s %2s %2s %2s %3s:%-3s %3s" % (team["position"], team["teamName"], team["playedGames"], team["wins"], team["draws"], team["losses"], team["goals"], team["goalsAgainst"], team["points"]))
