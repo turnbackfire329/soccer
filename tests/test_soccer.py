3033369b572224e0f37a9e5914776d6d47349dc4
Hendrik Hilleckes <hendrik.hilleckes@leogistics.com>
2017-09-18 22:21:47 +0200

    moved helper functions to data_connector, added season dc selection logic, added exceptions

--- tests/test_soccer.py
+++ tests/test_soccer.py
@@ -2,11 +2,12 @@
 
 from .context import soccer
 
-soc = soccer.Soccer(fd_apikey="***REMOVED***", db_path='soccerdb/database.sqlite')
+soc = soccer.Soccer(db_path='soccerdb/database_hallo.sqlite')
 writer = soccer.writers.Writer()
+fdo = soccer.data_connectors.FDOConnector()
 
 def test_season():
-    assert soc.get_current_season() == "2017"
+    assert soc._get_current_season() == 2017
     
 def test_table():
     standings = soc.get_league_table("BL1", season="2016", home=True, away=True)
@@ -21,14 +22,13 @@ def test_sort_descending():
         {"points": 5, "goalDifference": 4, "goals": 8},
         {"points": 5, "goalDifference": 5, "goals": 7}
     ]
-    assert soc.sort_league_table(standings, (soccer.Soccer.SORT_OPTIONS["POINTS"], soccer.Soccer.SORT_OPTIONS["DIFFERENCE"], soccer.Soccer.SORT_OPTIONS["GOALS"])) == [
+    assert fdo.sort_league_table(standings, (soccer.Soccer.SORT_OPTIONS["POINTS"], soccer.Soccer.SORT_OPTIONS["DIFFERENCE"], soccer.Soccer.SORT_OPTIONS["GOALS"])) == [
         {'goalDifference': 4, 'goals': 7, 'points': 11, 'position': 1},
         {'goalDifference': 4, 'goals': 7, 'points': 8, 'position': 2},
         {'goalDifference': 5, 'goals': 7, 'points': 5, 'position': 3},
         {'goalDifference': 4, 'goals': 8, 'points': 5, 'position': 4},
         {'goalDifference': 4, 'goals': 7, 'points': 5, 'position': 5}
     ]
-
 
 def test_table_restricted_teams():
     standings = soc.get_league_table("BL1", season="2016", home=True, away=True, teams=[4,5,6], head2headOnly=True)
